3187ac7ed883044332bbacff48d62130e83e2c50
smlee3845 <smlee3845@gmail.com>
2024-12-22 02:33:38 +0900

    Update plugin_loader.py

--- codegenie/plugin_loader.py
+++ codegenie/plugin_loader.py
@@ -1,1 +1,33 @@
+import importlib.util
+import os
+
+from .plugin import CodeGeniePlugin
+
+def load_plugins(plugin_directory):
+    """
+    Load all plugins from the specified directory.
+
+    Args:
+        plugin_directory (str): Path to the directory containing plugin files.
+
+    Returns:
+        list: List of loaded plugin instances.
+    """
+    plugins = []
+
+    for file_name in os.listdir(plugin_directory):
+        if file_name.endswith(".py") and not file_name.startswith("__"):
+            file_path = os.path.join(plugin_directory, file_name)
+            module_name = file_name[:-3]
+
+            spec = importlib.util.spec_from_file_location(module_name, file_path)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+
+            for attr_name in dir(module):
+                attr = getattr(module, attr_name)
+                if isinstance(attr, type) and issubclass(attr, CodeGeniePlugin) and attr is not CodeGeniePlugin:
+                    plugins.append(attr(attr_name))
+
+    return plugins
 
