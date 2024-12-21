687b85e42ae6399be66a1455ce67000ff095909d
smlee3845 <smlee3845@gmail.com>
2024-12-22 02:30:11 +0900

    Update __init__.py

--- codegenie/__init__.py
+++ codegenie/__init__.py
@@ -1,1 +1,14 @@
+"""
+CodeGenie Package Initialization
+"""
 
+# Import core functionalities for external usage
+from .formatter import format_code
+from .style_guide import generate_style_guide
+from .plugin_loader import load_plugins
+
+__all__ = [
+    "format_code",
+    "generate_style_guide",
+    "load_plugins"
+]
