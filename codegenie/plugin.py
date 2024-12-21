3baba8425387f5eea605e2043a622658127c1d2c
smlee3845 <smlee3845@gmail.com>
2024-12-22 02:33:12 +0900

    Update plugin.py

--- codegenie/plugin.py
+++ codegenie/plugin.py
@@ -1,1 +1,15 @@
+"""
+Plugin Base Class
+"""
+class CodeGeniePlugin:
+    """
+    Base class for CodeGenie plugins.
+    """
+    def __init__(self, name):
+        self.name = name
 
+    def execute(self, *args, **kwargs):
+        """
+        Execute the plugin functionality. To be implemented by subclasses.
+        """
+        raise NotImplementedError("Subclasses must implement the execute method.")
